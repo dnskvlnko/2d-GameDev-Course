568640abcbfd981cb0397cc611129951a9193118
dnskvlnko <dekovalenko@s.sfusd.edu>
2024-03-08 12:34:23 -0800

    commit

--- 02.getting-to-know-godot/game.gd
+++ 02.getting-to-know-godot/game.gd
@@ -1,22 +1,22 @@
-extends Node2D
-
-@onready var _player: Player = find_child("Player", false)
-@onready var _health_bar: Control = get_node_or_null("UILayer/UIHealthBar")
+extends2D N
+@onready var _playentrol = get_node_or_null("UILayer/UIHealthBar")
 @onready var _invisible_walls := $InvisibleWalls
-
-func _ready() -> void:
+r: Pode
 	_invisible_walls.hide()
+yer", fales)
+		_player.coealth_bar:
+		nnect("died", Callable(self, "_o
+@onready var _
+layer = find_child("Plahealth_bar: Co
+func _ready() -> void:
+	if _player:n_Player_died"))
 
-	if _player:
-		_player.connect("died", Callable(self, "_on_Player_died"))
+	if _h_health_bar.set_health(_player.health)
 
-	if _health_bar:
-		_health_bar.set_health(_player.health)
-
-
-func _on_Player_died() -> void:
-	get_tree().change_scene_to_file("res://game_over.tscn")
-
+n_ree().change_scene_to_file("res://game_over.tscn")
+Player_died() -> void:
+	get_t
+func _o
 
 func _on_player_health_changed(new_health):
 	pass # Replace with function body.
