0b4f244a5b619d31c4b2c1f01b00582a0045edfd
dnskvlnko <dekovalenko@s.sfusd.edu>
2024-03-26 09:10:54 -0700

    Revert "Merge branch 'main' into M4---To-Space-And-Beyond"
    This reverts commit fbeca08b7328f01cc80b8c53f2c1b25563213f93, reversing
    changes made to 2b22329d1e78dd2bc2f1766bf9aaa989ac10b472.

--- 02.getting-to-know-godot/game.gd
+++ 02.getting-to-know-godot/game.gd
@@ -1,22 +1,22 @@
-extends2D N
-@onready var _playentrol = get_node_or_null("UILayer/UIHealthBar")
+extends Node2D
+
+@onready var _player: Player = find_child("Player", false)
+@onready var _health_bar: Control = get_node_or_null("UILayer/UIHealthBar")
 @onready var _invisible_walls := $InvisibleWalls
-r: Pode
-	_invisible_walls.hide()
-yer", fales)
-		_player.coealth_bar:
-		nnect("died", Callable(self, "_o
-@onready var _
-layer = find_child("Plahealth_bar: Co
+
 func _ready() -> void:
-	if _player:n_Player_died"))
+	_invisible_walls.hide()
 
-	if _h_health_bar.set_health(_player.health)
+	if _player:
+		_player.connect("died", Callable(self, "_on_Player_died"))
 
-n_ree().change_scene_to_file("res://game_over.tscn")
-Player_died() -> void:
-	get_t
-func _o
+	if _health_bar:
+		_health_bar.set_health(_player.health)
+
+
+func _on_Player_died() -> void:
+	get_tree().change_scene_to_file("res://game_over.tscn")
+
 
 func _on_player_health_changed(new_health):
 	pass # Replace with function body.
