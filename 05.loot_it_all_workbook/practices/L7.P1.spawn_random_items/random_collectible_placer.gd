c041ab1412f62a72768db43bc5ed4c5dfed866a7
dnskvlnko <dekovalenko@s.sfusd.edu>
2024-04-18 12:56:27 -0700

    L7.P1

--- 05.loot_it_all_workbook/practices/L7.P1.spawn_random_items/random_collectible_placer.gd
+++ 05.loot_it_all_workbook/practices/L7.P1.spawn_random_items/random_collectible_placer.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
 var collectible_scenes := [
+	preload("coin.tscn"),
+	preload("energy_pack.tscn")
 ]
-
 
 func _ready() -> void:
 	get_node("Timer").timeout.connect(_on_timer_timeout)
 
-
-func _on_timer_timeout() -> void:
-	pass
+func _on_timer_timeout () -> void:
+	var random_item_scene: PackedScene = collectible_scenes.pick_random()
+	var item_instance := random_item_scene.instantiate()
+	add_child(item_instance)
